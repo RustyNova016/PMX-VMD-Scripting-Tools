08259d5842400bcbfd92d2a97307680a55065591
RustyNova <rusty.nova.jsb@gmail.com>
2022-05-12 18:54:00 +0200

    ask_for_image_file.py give absolute path

--- mmd_scripting/core/tools/ask_for_image_file.py
+++ mmd_scripting/core/tools/ask_for_image_file.py
@@ -1,9 +1,12 @@
+import os
 import mmd_scripting.core.nuthouse01_core as core
 
 
 def ask_for_image_file() -> str:
-    """
-    Ask the user for a image file.
+    """Ask the user for a image file.
+
+    @return: The image file's absolute path
     """
     core.MY_PRINT_FUNC("Please enter name of an image file:")
-    return core.MY_FILEPROMPT_FUNC("Image file", [".png", ".tga", ".jpg", ".jpeg", ".bmp", ".tiff", ".tif", ".gif"])
+    file_path = core.MY_FILEPROMPT_FUNC("Image file", [".png", ".tga", ".jpg", ".jpeg", ".bmp", ".tiff", ".tif", ".gif"])
+    return os.path.normpath(os.path.abspath(file_path))
